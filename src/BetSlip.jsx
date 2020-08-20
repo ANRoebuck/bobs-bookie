93dd446f04f6d0a384b7cc09049821a83136f5b9
anroebuck <aroebuck@quantbet.com>
2020-08-20 15:15:33 +0100

    add uuids to stake and place bet

--- src/BetSlip.jsx
+++ src/BetSlip.jsx
@@ -21,8 +21,18 @@ const BetSlip = ({selectedPrice, setShowBetslip}) => {
     <div className="bet-slip">
       <div className="backing">{backing}</div>
       <div className="odds">{odds}</div>
-      <input className="stake-field" type="text" value={stake} onChange={(e) => handleChange(e)}/>
-      <button type="button" onClick={() => mockPlaceBet()}>Place Bet</button>
+      <input
+        className="stake-field"
+        type="text" value={stake}
+        onChange={(e) => handleChange(e)}
+        data-qb-uuid="ff126b42-e2ee-11ea-87d0-0242ac130003"/>
+      <button
+        type="button"
+        onClick={() => mockPlaceBet()}
+        data-qb-uuid="2be4cc5a-e2ef-11ea-87d0-0242ac130003"
+      >
+        Place Bet
+      </button>
       <div className="description">{match}</div>
       <button type="button" onClick={() => setShowBetslip(false)}>Cancel</button>
     </div>
